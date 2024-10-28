f6a23a4daa7ecedab00f0c411c296a5316d81b13
Furkan Kümüş <105515121+Furkan-Kumus@users.noreply.github.com>
2024-10-28 21:33:57 +0300

    structure changed

---
+++ apart-ev-backend/src/test/java/fr/codecake/apart_ev_backend/ApartEvBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.codecake.apart_ev_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApartEvBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
